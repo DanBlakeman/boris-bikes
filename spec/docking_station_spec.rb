9d8705b15d629b5d5c141e3150eddabbfe9f5a0f
Dan Blakeman <DanBlakeman@users.noreply.github.com>
2015-03-22 18:28:01 +0000

    Updated DockingStation tests to test can give capacity on initialisation

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,1 +1,9 @@
 require 'docking_station'
+
+describe DockingStation do
+  subject { described_class.new(capacity: 123) }
+
+  it 'can set a default capacity on initialising' do
+    expect(subject.capacity).to eq(123)
+  end
+end
